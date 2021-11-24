83290aa54a113f12cc9660605677c28c573278a7
Goodness Adelaja <lajaololade@gmail.com>
2021-11-24 17:04:42 +0100

    feat:6.2: unicafe revisited, step2

--- src/reducer.js
+++ src/reducer.js
@@ -1,23 +1,26 @@
 const initialState = {
   good: 0,
   ok: 0,
-  bad: 0
-}
+  bad: 0,
+};
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
+  console.log(action);
   switch (action.type) {
-    case 'GOOD':
-      return state
-    case 'OK':
-      return state
-    case 'BAD':
-      return state
-    case 'ZERO':
-      return state
-    default: return state
+    case "GOOD":
+      const goodstate = state.good + 1;
+      return { ...state, good: goodstate };
+    case "OK":
+      const okstate = state.ok + 1;
+      return { ...state, ok: okstate };     
+    case "BAD":
+      const badstate = state.bad + 1;
+      return { ...state, bad: badstate };
+    case "ZERO":
+      return { good: 0, ok: 0, bad: 0 };    
+    default:
+      return state;
   }
-  
-}
+};
 
-export default counterReducer
+export default counterReducer;  
